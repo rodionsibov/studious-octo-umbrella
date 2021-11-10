6ae9784925fcd8a2bc98dfcfa593e95a84fc1f37
Rodion Sibov <rodionsibov@gmail.com>
2021-11-10 15:28:05 +0200

    init

---
+++ src/views/About.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="m-auto my-5">
+    Developed by
+    <a href="https://github.com/rodionsibov"> Rodion Sibov</a> &bull;
+    {{ date }}
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    date() {
+      return new Date().getFullYear();
+    },
+  },
+};
+</script>
