02829484afa3010df2d5b680c2e2ec850517da5b
Rodion Sibov <rodionsibov@gmail.com>
2021-11-10 23:45:25 +0200

    update

--- src/router/index.js
+++ src/router/index.js
@@ -10,12 +10,12 @@ const routes = [
   {
     path: '/projects',
     name: 'Projects',
-    component: () => import('@/components/Projects.vue')
+    component: () => import('@/views/Projects.vue')
   },
   {
     path: '/projects/:id',
     name: 'Project',
-    component: () => import('../views/Project.vue')
+    component: () => import('@/views/Project.vue')
   },
   {
     path: '/about',
