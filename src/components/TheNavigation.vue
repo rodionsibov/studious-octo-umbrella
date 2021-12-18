9dff6a36a1f318c61d1e5703751cd55060ecdf2e
Rodion Sibov <rodionsibov@gmail.com>
2021-12-18 15:54:49 +0200

    init

---
+++ src/components/TheNavigation.vue
@@ -0,0 +1,64 @@
+<template>
+  <nav
+    class="
+      my-20
+      md:mt-0
+      text-center
+      flex flex-col
+      gap-4
+      justify-between
+      md:flex-row
+    "
+  >
+    <div class="flex flex-col gap-4 md:flex-row">
+      <router-link
+        v-for="page in pages"
+        :key="page"
+        :to="{ name: page }"
+        class="hover:underline"
+      >
+        {{ page }}
+      </router-link>
+    </div>
+    <router-link
+      :to="{ name: 'Log In' }"
+      class="
+        flex
+        items-center
+        justify-center
+        gap-2
+        text-yellow-300
+        hover:underline
+      "
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1"
+        />
+      </svg>
+      Log In
+    </router-link>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      pages: ["Home", "Projects", "About"],
+    };
+  },
+};
+</script>
+
+<style>
+</style>
